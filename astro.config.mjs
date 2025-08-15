08bd789240c1b2465d3d4c9a833442c02d9920fc
hiina <hiina@hiina.space>
2025-08-14 18:24:55 -0600

    setup github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jam.vrg.party",
   integrations: [
     starlight({
       title: "jam.vrg.party",
